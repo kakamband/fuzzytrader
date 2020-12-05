c87bbc5440784d673cce9a371588ea78a7cba9bb
ricardofonsecacampos <34239257+ricardofonsecacampos@users.noreply.github.com>
2020-12-05 15:59:06 -0300

    Create fuzzytrader.test.js

---
+++ tests/fuzzytrader.test.js
@@ -0,0 +1,11 @@
+const math = require('../fuzzytrader')
+
+test('Total calculation', () => {
+	const total = math(10,0.3)
+	expect(total).toBe(13)
+})
+
+test('Total calculation with default tip', () => {
+	const total = math(10)
+	expect(total).toBe(12)
+})
